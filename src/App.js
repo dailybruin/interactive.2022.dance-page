ecdeed59b833383b748f38d6054cb770a763d824
markbenliyan <markbenliyan@g.ucla.edu>
2022-01-02 21:35:32 -0800

    finished mine and Cami's component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React, { useState, useEffect } from "react";
 import styled from 'styled-components';
 import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import PictureRow from "./components/PictureRow";
 
 
 
 function App() {
-  const [ data, setData ] = useState(null);
-  
-  useEffect(() => {
-		fetch("<TODO: insert api url here>")
-		.then(res => res.json())
-		.then(res => setData(res.data['article.aml']))
-  }, [])
 
   return (
     <div className="App">
-      <Header/>
-      Hello Daily Bruin!
-      <PictureRow/>
-      <Footer/>
+      <PictureRow />
     </div>
   );
 }
