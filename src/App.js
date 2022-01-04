39342d7767d4736858c3ab6c99e9049b0e3369f6
markbenliyan <markbenliyan@g.ucla.edu>
2022-01-03 18:50:24 -0800

    added mobile support & conditional statement to detect if mobile or not

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, { useState, useEffect } from "react";
 import styled from 'styled-components';
 import './App.css';
 import PictureRow from "./components/PictureRow";
+import PictureRowMobile from './components/PictureRowMobile'
 
 
 
 function App() {
-
+  const media = window.matchMedia('(max-width: 450px)');
+  const [isMobile, setIsMobile] = useState(media.matches);
   return (
     <div className="App">
-      <PictureRow />
+      { !isMobile && <PictureRow />}
+      { isMobile && <PictureRowMobile />}
     </div>
   );
 }
