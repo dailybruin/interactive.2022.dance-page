420a19deb139ac8bd3c292759c4edc6aba728ca6
Tracy Zhao <75452724+TracyZhao24@users.noreply.github.com>
2022-01-04 00:35:00 -0800

    tried accessibility setting to get arrows

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -17,7 +17,8 @@ export default function Carousel() {
             infinite: true,
             speed: 500,
             slidesToShow: 3,
-            slidesToScroll: 3
+            slidesToScroll: 3,
+            accessibility: true
         };
     return (
         <div> 
