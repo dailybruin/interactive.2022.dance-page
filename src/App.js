49313618aa561027a3dece0765480816b6d87304
mattiesansev <mesansev@gmail.com>
2022-01-05 14:27:32 -0800

    simple video player

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
-
+import VideoPlayer from "./components/VideoPlayer";
 
 function App() {
   const [ data, setData ] = useState(null);
@@ -19,6 +18,7 @@ function App() {
     <div className="App">
       <Header/>
       Hello Daily Bruin!
+      <VideoPlayer />
       <Footer/>
     </div>
   );
---
+++ src/components/VideoPlayer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const VideoPlayer = () => (
+  <iframe 
+    width="90%" 
+    height="415" 
+    src="https://www.youtube.com/embed/j4ju_TWhKzA" 
+    title="YouTube video player" 
+    frameborder="0" 
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+    allowfullscreen></iframe>
+);
+
+
+export default VideoPlayer;
