7fbeb8e4823097cbad61c7b69f6dd5cf355739bf
mattiesansev <mesansev@gmail.com>
2022-01-05 14:33:20 -0800

    one more fix

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -4,9 +4,9 @@ import PropTypes from "prop-types";
 const VideoPlayer = () => (
   <iframe 
     width="95%" 
-    height="415" 
+    height="515" 
     src="https://www.youtube.com/embed/j4ju_TWhKzA" 
-    title="YouTube video player" 
+    title="Dance Disassembled" 
     frameborder="0" 
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
     allowfullscreen></iframe>
