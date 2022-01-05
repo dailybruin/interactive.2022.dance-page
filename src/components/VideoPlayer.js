063afd3b91d5978e3d391e4c7598a1c22edb83fb
mattiesansev <mesansev@gmail.com>
2022-01-05 14:30:10 -0800

    width is 95% i think that fits figma

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 const VideoPlayer = () => (
   <iframe 
-    width="90%" 
+    width="95%" 
     height="415" 
     src="https://www.youtube.com/embed/j4ju_TWhKzA" 
     title="YouTube video player" 
