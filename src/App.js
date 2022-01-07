c553ab93257c2df25314ae5b03fcbd4f423a5fe6
Jingheng Li <justsunli@Sunnys-MacBook-Pro.local>
2022-01-06 18:42:10 -0800

    arrange components

--- src/App.js
+++ src/App.js
@@ -95,19 +95,29 @@ function App() {
       <Header/>
       
       <Landing/>
-      {/* <StickySidebar/> */}
-      <Image />
-      { !isMobile && <PictureRow />}
-      { isMobile && <PictureRowMobile />}
       <IntroText>
       From selecting music and choreography to perfecting costumes and makeup, the ins and outs of putting on a dance performance are complex and detailed. Put on your dancing shoes and follow along as columnist Laura Carter takes a behind-the-scenes look at dance, disassembled.
       </IntroText>
+      
       {!isMobile &&<Container>
         <Left>
           <StickySidebar scroll={scroll}/>
         </Left>
 
         <Right>
+          <Illo/>
+          <PictureRow />
+          <Carousel images = {["https://i.insider.com/6164e6d62457a4001982d6d6?width=1000&format=jpeg&auto=webp",
+        "https://media1.popsugar-assets.com/files/thumbor/CvY77oKxMvbrpjd1-TazkhzFJ2U/fit-in/728xorig/filters:format_auto-!!-:strip_icc-!!-/2021/10/27/851/n/1922283/36730ec66179a7bfcdf596.95187525_/i/jojo-siwa-dancing-with-the-stars-performances-videos.jpg",
+        "https://www.billboard.com/wp-content/uploads/2021/11/Jojo-Siwa-dancing-with-the-stars-2021-billboard-1548-1636472738.jpg",
+        "https://s31242.pcdn.co/wp-content/uploads/2021/11/jojo-siwa-dancing-with-the-stars.jpg",
+        "https://static.parade.com/wp-content/uploads/2021/09/dancing-with-the-stars-jenna-johnson-jojo-siwa-160626_5765.jpg",
+        "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2021%2F10%2F13%2FJoJo-Siwa-DWTS.jpg"
+        ]}
+        photographer = "PHOTOGRAPHER"
+        caption = "Caption. Caption caption caption. Caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption"
+        ></Carousel>
+          <VideoPlayer />
           <TestingContainer>section1</TestingContainer>
           <TestingContainer>section2</TestingContainer>
 
@@ -123,22 +133,11 @@ function App() {
      
         </MobileContainer>}
      
-      <Illo />
-      { !isMobile && <PictureRow />}
-      { isMobile && <PictureRowMobile />}
-      <Carousel images = {["https://i.insider.com/6164e6d62457a4001982d6d6?width=1000&format=jpeg&auto=webp",
-        "https://media1.popsugar-assets.com/files/thumbor/CvY77oKxMvbrpjd1-TazkhzFJ2U/fit-in/728xorig/filters:format_auto-!!-:strip_icc-!!-/2021/10/27/851/n/1922283/36730ec66179a7bfcdf596.95187525_/i/jojo-siwa-dancing-with-the-stars-performances-videos.jpg",
-        "https://www.billboard.com/wp-content/uploads/2021/11/Jojo-Siwa-dancing-with-the-stars-2021-billboard-1548-1636472738.jpg",
-        "https://s31242.pcdn.co/wp-content/uploads/2021/11/jojo-siwa-dancing-with-the-stars.jpg",
-        "https://static.parade.com/wp-content/uploads/2021/09/dancing-with-the-stars-jenna-johnson-jojo-siwa-160626_5765.jpg",
-        "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F6%2F2021%2F10%2F13%2FJoJo-Siwa-DWTS.jpg"
-        ]}
-        photographer = "PHOTOGRAPHER"
-        caption = "Caption. Caption caption caption. Caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption caption"
-        ></Carousel>
+  
+      
 
       
-      <VideoPlayer />
+     
       <Footer/>
       
     </div>
